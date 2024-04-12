1485e9f898b13c4a4b327dd5091595d9ca185435
Pavelitell <156504737+Pavelitell@users.noreply.github.com>
2024-04-12 13:52:16 +0300

    адеюсь последние исправления

--- Maven_Module_SDK18/src/test/java/helpers/ImageManager.java
+++ Maven_Module_SDK18/src/test/java/helpers/ImageManager.java
@@ -16,14 +16,14 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ImageManager extends BaseTest {
 
-    public int actual = (int)(Math.random()*10);
-    public int expected = (int)(Math.random()*10);
+    public int actual = (int) (Math.random() * 10);
+    public int expected = (int) (Math.random() * 10);
 
-    public static void downloadExpectedImage(Object image, int generatedNameNumber){
+    public static void downloadExpectedImage(Object image, int generatedNameNumber) {
         /* мой путь  с генерированным названием*/
-        String downloadPath =  "C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Downloads" +(generatedNameNumber)+".png";
+        String downloadPath = "C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Downloads" + (generatedNameNumber) + ".png";
         try {
-            URL url = new URL((String)  image);
+            URL url = new URL((String) image);
             InputStream input = url.openStream();
             OutputStream output = new FileOutputStream(downloadPath);
 
@@ -39,6 +39,7 @@ public class ImageManager extends BaseTest {
             e.printStackTrace();
         }
     }
+
     public static void downloadActualImage(Object image, int generatedNameNumber) {
 
 
@@ -62,29 +63,30 @@ public class ImageManager extends BaseTest {
         }
 
     }
-        public static void screenShoting(int generatedNameNumber) throws InterruptedException {
-        Thread.sleep(150);
-            TakesScreenshot screenshot = (TakesScreenshot) driver ;
-            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-            /* мой путь  с генерированным названием*/
-            File targetFile = new File ("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Screenshots\\screenshot" +(generatedNameNumber)+".png");
-            try {
-                FileUtils.copyFile(srcFile, targetFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-<<<<<<< HEAD
-    public static void compareImage(int actualInt, int expectedInt,String packageName,String fileName ){
-=======
-public static void compareImage(int actualInt, int expectedInt,String packageName,String fileName ){
->>>>>>> c406fc3d822730a96593a339677318407b2225f8
-    BufferedImage expectedImage = ImageComparisonUtil.readImageFromResources("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\"+ packageName+"\\" +(fileName)+(actualInt)+".png");
-    BufferedImage actualImage = ImageComparisonUtil.readImageFromResources("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\"+ packageName +"\\"+ (fileName)+(expectedInt)+".png");
 
-    File resultDestination = new File( "C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Results\\result"+(actualInt)+(expectedInt)+".png" );
-    ImageComparisonResult imageComparisonResult = new ImageComparison(expectedImage, actualImage).compareImages();
-    assertNotEquals(ImageComparisonState.MATCH, imageComparisonResult.getImageComparisonState());
-    ImageComparisonUtil.saveImage(resultDestination, imageComparisonResult.getResult());
+    public static void screenShoting(int generatedNameNumber) throws InterruptedException {
+        Thread.sleep(150);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+        /* мой путь  с генерированным названием*/
+        File targetFile = new File("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Screenshots\\screenshot" + (generatedNameNumber) + ".png");
+        try {
+            FileUtils.copyFile(srcFile, targetFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void compareImage(int actualInt, int expectedInt, String packageName, String fileName) {
+
+
+        BufferedImage expectedImage = ImageComparisonUtil.readImageFromResources("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\" + packageName + "\\" + (fileName) + (actualInt) + ".png");
+        BufferedImage actualImage = ImageComparisonUtil.readImageFromResources("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\" + packageName + "\\" + (fileName) + (expectedInt) + ".png");
+
+        File resultDestination = new File("C:\\Users\\kras8\\OneDrive\\Рабочий стол\\AQA\\ImageManager\\Results\\result" + (actualInt) + (expectedInt) + ".png");
+        ImageComparisonResult imageComparisonResult = new ImageComparison(expectedImage, actualImage).compareImages();
+        assertNotEquals(ImageComparisonState.MATCH, imageComparisonResult.getImageComparisonState());
+        ImageComparisonUtil.saveImage(resultDestination, imageComparisonResult.getResult());
+    }
 }
-}
+
